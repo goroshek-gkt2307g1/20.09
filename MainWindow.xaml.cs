c0933841580838536849540da99dff69e260f100
goroshek-gkt2307g1 <vlasovaaa@mer.ci.nsu.ru>
2025-09-20 10:47:52 +0700

    2 задание

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,9 +18,23 @@ namespace _20._09
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RunButton(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            double maxLeft = MainCanvas.ActualWidth - runButton.ActualWidth;
+            double maxTop = MainCanvas.ActualHeight - runButton.ActualHeight;
+
+            double randomLeft = random.NextDouble() * maxLeft;
+            double randomTop = random.NextDouble() * maxTop;
+
+            leftAnimation.To = randomLeft;
+            topAnimation.To = randomTop;
         }
     }
 }
